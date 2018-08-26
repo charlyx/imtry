6348375aa2ee6dba042fc5d406f97c57dc67ade1
Kseniia Saievych <kseniia@abtasty.com>
2018-08-26 17:08:14 +0200

    :sparkles: Add getClosestTramwayAt

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fetch = require('node-fetch')
 
+const TRAMWAY = 1
+
+const TERMINUS_1 = 1
+const TERMINUS_2 = 2
+
 async function getCodeLieu(trainStation) {
   const response = await fetch('http://open_preprod.tan.fr/ewp/arrets.json')
 
@@ -18,4 +23,43 @@ async function getCodeLieu(trainStation) {
   return arret.codeLieu
 }
 
-module.exports = { getCodeLieu }
+async function getClosestTramwayAt(codeLieu) {
+  const response = await fetch(`http://open_preprod.tan.fr/ewp/tempsattente.json/${codeLieu}`)
+
+  if (!response.ok) {
+    throw Error('Service unavailable')
+  }
+
+  const tempsAttente = await response.json()
+
+  const tramTempsAttente = tempsAttente.filter(({ ligne }) => ligne.typeLigne === TRAMWAY )
+
+  if (!tramTempsAttente) {
+    throw Error(`No trains at station ${codeLieu} at the moment`)
+  }
+  
+  const sortedTempsAttente = tramTempsAttente.sort((a, b) => {
+    const tempsA = getTempsAsNumber(a.temps)
+    const tempsB = getTempsAsNumber(b.temps)
+
+    return tempsA - tempsB
+  })
+
+  const firstDirection = sortedTempsAttente.find(({ sens }) => sens === TERMINUS_1 )
+  const secondDirection = sortedTempsAttente.find(({ sens }) => sens === TERMINUS_2 )
+
+  return {
+    [firstDirection.terminus]: getTempsAsNumber(firstDirection.temps),
+    [secondDirection.terminus]: getTempsAsNumber(secondDirection.temps),
+  }
+}
+
+function getTempsAsNumber(temps) {
+  return parseInt(temps.replace(/ mn.*/, ''))
+}
+
+module.exports = { 
+  getCodeLieu,
+  getTempsAsNumber, 
+  getClosestTramwayAt 
+}
--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,5 @@
-const { getCodeLieu } = require('.')
+const nock = require('nock')
+const { getCodeLieu, getTempsAsNumber, getClosestTramwayAt } = require('.')
 
 const TRAIN_STATION = 'Croix Bonneau'
 
@@ -21,4 +22,48 @@ describe('getCodeLieu', () => {
   })
 })
 
+describe('getTempsAsNumber', () => {
+  it('should return number of minutes', () => {
+    expect(getTempsAsNumber('3 mn')).toBe(3)
+  })
+  it('should return number of minutes and ignore seconds', () => {
+    expect(getTempsAsNumber('3 mn 30')).toBe(3)
+  })
+})
+
+describe('getClosestTramwayAt', () => {
+  beforeAll(mockTempsAttente)
+
+  it('should return closest tramway in both directions at XBON', async () => {
+    const codeLieu = 'XBON'
+    const tempsAttente = await getClosestTramwayAt(codeLieu)
+
+    expect(tempsAttente).toEqual({ Jamet: 3, Bouffay: 5 })    
+  })
+
+})
+
+function mockTempsAttente() {
+  nock('http://open_preprod.tan.fr')
+    .get('/ewp/tempsattente.json/XBON')
+    .reply(200, [
+      {"sens":1,"terminus":"Jamet","infotrafic":true,"temps":"3 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON2"}},
+      {"sens":2,"terminus":"Perray","infotrafic":true,"temps":"4 mn 30","dernierDepart":"false","tempsReel":"true","ligne":{"numLigne":"11","typeLigne":3},"arret":{"codeArret":"XBON4"}},
+      {"sens":2,"terminus":"Bouffay","infotrafic":true,"temps":"5 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON1"}},
+      {"sens":1,"terminus":"François Mitterrand","infotrafic":true,"temps":"11 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON2"}},
+      {"sens":2,"terminus":"Bouffay","infotrafic":true,"temps":"13 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON1"}},
+      {"sens":1,"terminus":"Jamet","infotrafic":true,"temps":"20 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON2"}},
+      {"sens":2,"terminus":"Bouffay","infotrafic":true,"temps":"21 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON1"}},
+      {"sens":1,"terminus":"Tertre","infotrafic":false,"temps":"26 mn","dernierDepart":"false","tempsReel":"true","ligne":{"numLigne":"11","typeLigne":3},"arret":{"codeArret":"XBON3"}},
+      {"sens":1,"terminus":"François Mitterrand","infotrafic":true,"temps":"28 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON2"}},
+      {"sens":2,"terminus":"Bouffay","infotrafic":true,"temps":"29 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON1"}},
+      {"sens":1,"terminus":"Jamet","infotrafic":true,"temps":"36 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON2"}},
+      {"sens":2,"terminus":"Bouffay","infotrafic":true,"temps":"37 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON1"}},
+      {"sens":2,"terminus":"Perray","infotrafic":true,"temps":"39 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"11","typeLigne":3},"arret":{"codeArret":"XBON4"}},
+      {"sens":1,"terminus":"François Mitterrand","infotrafic":true,"temps":"44 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON2"}},
+      {"sens":2,"terminus":"Bouffay","infotrafic":true,"temps":"45 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON1"}},
+      {"sens":1,"terminus":"Jamet","infotrafic":true,"temps":"52 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON2"}},
+      {"sens":2,"terminus":"Bouffay","infotrafic":true,"temps":"53 mn","dernierDepart":"false","tempsReel":"false","ligne":{"numLigne":"1","typeLigne":1},"arret":{"codeArret":"XBON1"}},
+    ])
+} 
 
