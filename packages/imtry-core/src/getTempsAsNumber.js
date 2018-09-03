614cc800025445c73d4d7f5252ae5fd14da77ee4
Charles-Henri Guérin <charles-henri.guerin@zenika.com>
2018-09-03 20:18:24 +0200

    :bug: Return 0 when departure is close

--- packages/imtry-core/src/getTempsAsNumber.js
+++ packages/imtry-core/src/getTempsAsNumber.js
@@ -1,3 +1,7 @@
 export function getTempsAsNumber(temps) {
+  if (temps === 'horaire.proche') {
+    return 0
+  }
+
   return parseInt(temps.replace(/ mn.*/, ''))
 }
--- packages/imtry-core/src/getTempsAsNumber.spec.js
+++ packages/imtry-core/src/getTempsAsNumber.spec.js
@@ -4,4 +4,8 @@ describe('getTempsAsNumber', () => {
   it('should return number of minutes', () => {
     expect(getTempsAsNumber('3 mn 30')).toBe(3)
   })
+
+  it('should return 0 when departure is close', () => {
+    expect(getTempsAsNumber('horaire.proche')).toBe(0)
+  })
 })
