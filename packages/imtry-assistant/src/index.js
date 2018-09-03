d4bc4fff5ece04cfd161c0d50117523f211bb07e
Charles-Henri Guérin <charles-henri.guerin@zenika.com>
2018-09-03 16:53:27 +0200

    :sparkles: Answer to ClosestTram intent

--- packages/imtry-assistant/src/index.js
+++ packages/imtry-assistant/src/index.js
@@ -1,24 +1,31 @@
 'use strict'
 
 const functions = require('firebase-functions')
-const { WebhookClient} = require('dialogflow-fulfillment')
+const { WebhookClient } = require('dialogflow-fulfillment')
+const { getClosestTramwayFrom } = require('@imtry/core')
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response })
-
-  function welcome(agent) {
-    agent.add('Welcome to my agent!')
-  }
-
-  function fallback(agent) {
-    agent.add('I didn\'t understand')
-    agent.add('I\'m sorry, can you try again?')
-  }
-
   const intentMap = new Map()
 
-  intentMap.set('Default Welcome Intent', welcome)
-  intentMap.set('Default Fallback Intent', fallback)
+  intentMap.set('ClosestTram', ClosestTramHandler)
 
   agent.handleRequest(intentMap)
 })
+
+function ClosestTramHandler(agent) {
+  const { station } = agent.parameters
+  return getClosestTramwayFrom(station).then(tramways => {
+    const responses = Object.keys(tramways).map(direction => `
+      Le prochain tramway au départ de ${station}
+      et en direction de ${direction}
+      partira dans ${tramways[direction]} minutes
+    `)
+
+    responses.forEach(response => agent.add(response))
+    return
+  }).catch(e => {
+    console.error(e)
+    agent.add('Désolé, j\'ai glissé chef.')
+  })
+}
